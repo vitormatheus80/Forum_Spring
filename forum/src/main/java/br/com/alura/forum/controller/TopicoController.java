24bff8a2a58570c0e7989d492826ebfba5734dfb
Vitor Matheus <vitormatheus@pop-os.localdomain>
2022-01-31 19:01:12 -0300

    Finalização do CRUD

--- forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -2,14 +2,18 @@ package br.com.alura.forum.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualiazacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
@@ -54,11 +59,37 @@ public class TopicoController {
 		}
 	
 	@GetMapping("/{id}")
-	public DetalhesDoTopicoDto detalhar (@PathVariable Long id) {
-		Topico topico = topicoRepository.getById(id);
-		return new DetalhesDoTopicoDto(topico); }
+	public ResponseEntity<DetalhesDoTopicoDto> detalhar(@PathVariable Long id) {
+	Optional<Topico>  topico = topicoRepository.findById(id);
+	if (topico.isPresent()) {
+		return ResponseEntity.ok(new DetalhesDoTopicoDto(topico.get()));
+	}
+	return ResponseEntity.notFound().build();
+		 }
 	
-	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDto> atualizar (@PathVariable Long id, @RequestBody @Valid AtualiazacaoTopicoForm form) {
+		
+		Optional<Topico>  optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			Topico topico = form.atulizar(id, topicoRepository);
+			return ResponseEntity.ok(new TopicoDto(topico));
+		}
+		return ResponseEntity.notFound().build();
+			 }
+		
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> remover (@PathVariable Long id){
+		Optional<Topico>  optional = topicoRepository.findById(id);
+		if (optional.isPresent()) {
+			topicoRepository.deleteById(id);
+			return ResponseEntity.ok().build();
+		}
+		return ResponseEntity.notFound().build();
+		
+	}
 	}
 
 
